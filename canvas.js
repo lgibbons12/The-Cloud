20bcc2aa0609e8d024bbbf0c8cb8e452d4fff18d
lgibbons12 <lgibbons@cannonschool.org>
2022-12-05 13:45:07 -0500

    got the cloud looking great

--- canvas.js
+++ canvas.js
@@ -1,4 +1,4 @@
-//html5 canvas tutorial for beginners ep 3 15:20
+//html5 canvas tutorial for beginners ep 4 10:54
 //linkto: https://www.youtube.com/watch?v=yq2au9EfeRQ
 
 
@@ -12,8 +12,7 @@ canvas.height = window.innerHeight;
 //a bunch of methods stored in the variable
 var c = canvas.getContext('2d');
 
-/* c.fillStyle = "rgb(255, 230, 0)";
-c.fillRect(100, 100, 100, 50); */
+
 
 // Line
 /* c.beginPath();
@@ -48,28 +47,77 @@ c.stroke(); */
     c.arc(x, y, 30, 0, Math.PI * 2, false);
     c.stroke();
 } */
+var mouse = {
+    x: undefined,
+    y: undefined
+}
+window.addEventListener("mousemove", function (event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+})
+class Circle {
+    constructor(x, y, dx, dy, radius) {
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+        this.radius = radius;
 
-var x = Math.random() * innerWidth;
-var y = Math.random() * innerHeight;
-var dx = (Math.random() - 0.5) * 5;
-var dy = (Math.random() - 0.5) * 5;
-var radius = 30;
+        this.draw = function () {
+            c.beginPath();
+            c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+            c.strokeStyle = "gray";
+            c.stroke();
+            c.fillStyle = "gray";
+            c.fill();
+        };
+        this.update = function () {
+            if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+                this.dx *= -1;
+            }
+            if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+                this.dy *= -1;
+            }
+            this.x += this.dx;
+            this.y += this.dy;
+
+            //interactivity
+            if (mouse.x - this.x < 25 && mouse.x - this.x > - 25 && 
+                mouse.y - this.y < 25 && mouse.y - this.y > -25 ) {
+                this.radius += 1;
+            }
+            
+            else if (this.radius > 2){
+                this.radius -= 1;
+            }
+        };
+    }
+}
+
+var circleArray = [];
+for (var i = 0; i < 100; i++) {
+    var x = Math.random() * innerWidth;
+    var y = Math.random() * innerHeight;
+    var dx = 0;
+    var dy = dx = (Math.random() -0.5) * 8;
+    var radius = 30;
+    circleArray.push(new Circle(x, y, dx, dy, radius))
+}
+
+
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, innerWidth, innerHeight);
+    c.fillStyle = "rgb(137, 196, 244)";
+    c.fillRect(0, 0, innerWidth, innerHeight); 
+
+    c.fillStyle = "yellow";
+    c.strokeStyle = "yellow";
+
     c.beginPath();
-    c.arc(x, y, 30, 0, Math.PI * 2, false);
-    c.strokeStyle = "blue";
-    c.stroke();
+    c.moveTo(S)
     
-    if (x + radius > innerWidth || x - radius < 0) {
-        dx *= -1;
-    }
-    if (y + radius > innerHeight || y - radius < 0) {
-        dy *= -1;
-    }
-    x += dx;
-    y += dy;
+    
 }
 
 animate();
---
+++ cloud.js
@@ -0,0 +1,148 @@
+//get the canvas from DOM
+var canvas = document.querySelector("canvas");
+
+//state of website
+var webState = 2;
+
+//make the canvas take up the whole screen
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+//a bunch of methods stored in the variable
+var c = canvas.getContext('2d');
+
+
+drawClouds = function(x, y, size) {
+    c.beginPath();
+    c.arc(x, y, size, 0, Math.PI, true);
+    c.strokeStyle = "white";
+    c.stroke();
+    c.fillStyle = "white";
+    c.fill();
+}
+class Cloud {
+    constructor(x, y, dx, size) {
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.size = size;
+        this.offScreen = false;
+
+
+        this.draw = function() {
+            drawClouds(this.x - (this.size * .8), this.y, this.size);
+            drawClouds(this.x + (this.size * 1.5), this.y, this.size * 2);
+            drawClouds(this.x + (this.size * 3.5), this.y, this.size * 1.5);
+            
+            
+        }
+        this.update = function () {
+            if (this.x < 0 || this.x > innerWidth) {
+                this.offScreen = true;
+            }
+            
+            this.x += this.dx;
+        }
+    }
+}
+var cloudArray = [ 
+    new Cloud(innerWidth/1.9, innerHeight/5 + 2, 2, 40),
+    new Cloud(innerWidth/2.1, innerHeight/5 + 2, -2, 40)
+];
+
+var mainCloud = new Cloud(innerWidth/3, innerHeight/4, 0, 80);
+
+var mouse = {
+    x: undefined,
+    y: undefined,
+    click: undefined
+}
+window.addEventListener("mousemove", function (event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+})
+
+window.addEventListener("click", function(event) {
+    mouse.click = true;
+    mouse.x = event.x;
+    mouse.y = event.y;
+})
+
+function animate() {
+    //normal animation resets
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, innerWidth, innerHeight);
+    c.fillStyle = "rgb(137, 196, 244)";
+    c.fillRect(0, 0, innerWidth, innerHeight); 
+    //opening animation
+    if (webState === 0) {
+        c.font = "70px Arial";
+        c.fillStyle = "black"
+        c.fillText("The Cloud", innerWidth/2 - (innerWidth/5), innerHeight/5);
+        for (let i = 0; i < cloudArray.length; i++) {
+            cloudArray[i].draw();
+            cloudArray[i].update();
+        }
+
+        let counter = 0;
+        for (let i = 0; i < cloudArray.length; i++) {
+            if (cloudArray[i].offScreen === true) {
+                counter++;
+            }
+        }
+        if (counter === cloudArray.length) {
+            webState = 1;
+        }
+    }
+    else if (webState === 1) {
+        c.fillStyle = "white";
+        c.fillText("Enter the Cloud", innerWidth/2 - (innerWidth/4), innerHeight/3);
+        if (mouse.click) {
+            if (mouse.x > (innerWidth/2 - (innerWidth/4)) && mouse.x < ((innerWidth/2 - (innerWidth/4)) + innerWidth/2 + 10)) {
+                if (mouse.y > innerHeight/4 && mouse.y < (innerHeight/4) + innerHeight/4) {
+                    webState = 2;
+                }
+            }
+        }
+    }
+    else if (webState === 2) {
+
+        //lightning
+        c.beginPath();
+        c.lineWidth = 20;
+        c.moveTo(innerWidth/4, innerHeight/4.2);
+        c.lineTo(innerWidth / 3, innerHeight/3);
+        c.lineTo(innerWidth/3.9, innerHeight/3.2);
+        c.lineTo(innerWidth/2.8, innerHeight/2.2);
+        c.stroke();
+
+        //main cloud
+        c.strokeStyle = "white";
+        c.lineWidth = 1;
+        mainCloud.draw();
+        c.fillStyle = "yellow";
+        c.strokeStyle = "yellow";
+        c.fillStyle = "black";
+        c.font = "15px Garamond";
+        c.fillText("Deployment Methods", innerWidth/4.7, innerHeight/4.5);
+        c.fillText("Services", innerWidth/1.6, innerHeight/4.5);
+        c.font = "30px Garamond";
+        c.fillText("How It Works", innerWidth/2.7, innerHeight/5.5);
+        c.rotate(-60);
+        c.font = "15px Garamond";
+        c.fillText("Major Use Cases", innerWidth, innerHeight/4.5);
+        c.rotate(60);
+
+        
+        
+
+    }
+
+    
+
+    
+    
+    
+}
+
+animate();
